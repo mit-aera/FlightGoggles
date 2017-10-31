d4b778f6e4a57a8f0c609df02de641e47e88e2c2
Winter Guerra <winterg@mit.edu>
2017-10-31 16:39:44 -0400

    Still refactoring. Focusing on the image serialization step now.

--- FlightGoggles/Scripts/CameraController.cs
+++ FlightGoggles/Scripts/CameraController.cs
@@ -43,12 +43,9 @@ public class CameraController : MonoBehaviour
     private StateMessage_t state;
 
     // Internal state & storage variables
-    private Dictionary<string, GameObject> objects;
+    private UnityState_t internal_state;
     private Texture2D rendered_frame;
     private object socket_lock;
-    private bool screen_initialized = false;
-    private bool cameras_initialized = false;
-    private bool connected = false;
 
 
     // Helper function for getting command line arguments
@@ -68,42 +65,7 @@ public class CameraController : MonoBehaviour
     // Helper functions for converting list -> vector
     public static Vector3 ListToVector3(IList<float> list) { return new Vector3(list[0], list[1], list[2]); }
     public static Quaternion ListToQuaternion(IList<float> list) { return new Quaternion(list[0], list[1], list[2], list[3]); }
-
-    /* Use this for initialization
-     * Should create a worker thread that listens for ZMQ messages and saves the most recent one.
-     */
-
-    private void ensureScreenSize()
-    {
-        if (!screen_initialized && connected) {
-            // Set the max framerate
-            Application.targetFrameRate = max_framerate;
-            // initialize the display to a window that fits all cameras
-            Screen.SetResolution(state.rendered_image_width, state.rendered_image_height, false);
-            // Set render texture to the correct size
-            rendered_frame = new Texture2D(state.rendered_image_width, state.rendered_image_height, TextureFormat.RGB24, false, true);
-            // Screen is initialized, but the cameras are not.
-            screen_initialized = true;
-            return;
-        }
-        // Initialize the cameras (on next frame)
-        if (screen_initialized && connected && !cameras_initialized){
-            // Make sure that all cameras are drawing to the correct portion of the screen.
-            foreach (Camera_t cam in state.Cameras)
-            {
-                // Get object
-                GameObject camera_obj = cam.get_gameobject(objects, camera_template);
-                // Make sure camera renders to the correct portion of the screen.
-                camera_obj.GetComponent<Camera>().pixelRect = new Rect(0, state.cam_height*(state.num_cameras - cam.output_index - 1), state.cam_width, state.cam_height);
-                // Ensure FOV is set for camera.
-                camera_obj.GetComponent<Camera>().fieldOfView = state.cam_vertical_fov;
-                // enable Camera.
-                camera_obj.SetActive(true);
-            }
-            cameras_initialized = true;
-
-        }
-    }
+    public static Color ListHSVToColor(IList<float> list) { return Color.HSVToRGB(list[0], list[1], list[2]); }
 
     public IEnumerator Start()
     {
@@ -135,8 +97,8 @@ public class CameraController : MonoBehaviour
         push_socket.Connect(video_host);
         Debug.Log("Sockets bound.");
 
-        // Initialize Objects
-        objects = new Dictionary<string, GameObject>() { };
+        // Initialize Internal State
+        internal_state = new UnityState_t();
 
 
         // Wait until end of frame to transmit images
@@ -146,11 +108,12 @@ public class CameraController : MonoBehaviour
             // Blocks until the frame is rendered.
             yield return new WaitForEndOfFrame();
 
-            if (screen_initialized && cameras_initialized)
+            // Check if this frame should be rendered.
+            if (internal_state.screen_initialized && internal_state.screen_skip_frames == 0)
             {
 
-                // Read pixels from the display.
-                rendered_frame.ReadPixels(new Rect(0, 0, state.rendered_image_width, state.rendered_image_height), 0, 0);
+                // Read pixels from screen backbuffer (expensive).
+                rendered_frame.ReadPixels(new Rect(0, 0, state.screen_width, state.screen_height), 0, 0);
                 rendered_frame.Apply();
                 byte[] raw = rendered_frame.GetRawTextureData();
 
@@ -158,65 +121,27 @@ public class CameraController : MonoBehaviour
                 // Compress and send the image in a different thread (if using windows).
                 Task.Run(() =>
                 {
-                    const int channels = 3;
+                    // Get metadata
+                    RenderMetadata_t metadata = new RenderMetadata_t(state);
 
-                    // Get the grayscale image, flipping the rows such that the image is transmitted rightside up
-                    // @TODO: Use REAL grayscale conversion.
-                    byte[] raw_flipped_gray = new byte[raw.Length/channels];
-
-                    // Temp vars
-                    byte R, G, B;
-
-                    for (int y = 0; y < state.rendered_image_height; y++)
-                    {
-                        int y_inv = state.rendered_image_height - y - 1;
-                        for (int x = 0; x < state.rendered_image_width; x++)
-                        {
-                            R = raw[((y * state.rendered_image_width) + x) * channels];
-                            G = raw[((y * state.rendered_image_width) + x) * channels + 1];
-                            B = raw[((y * state.rendered_image_width) + x) * channels + 2];
-
-                            // Grayscale conversion.
-                            // https://www.mathworks.com/help/matlab/ref/rgb2gray.html
-                            // Use of Math.Min for overflow handling without try/catch statement.
-                            // NOTE: This does not round correctly (just truncates), since rounding is SLOW (-10FPS)
-                            raw_flipped_gray[(y_inv * state.rendered_image_width) + x] = (byte) Math.Min(0.2989 * R + 0.5870 * G + 0.1140 * B, byte.MaxValue);
-                        }
-                    }
-
-                    // Create the image data holder
-                    byte[] image;
-                    if (should_compress_video)
-                    {
-
-                        // Compress the image using the fastest system package available.
-#if (UNITY_EDITOR_WINDOWS || UNITY_STANDALONE_WINDOWS)
-                        image =  SnappyCodec.Compress(raw_flipped_gray);
-#else
-                        image = Snappy.Sharp.Snappy.Compress(raw_flipped_gray);
-#endif
-                    }
-                    else
-                    {
-                        image = raw_flipped_gray;
-                    }
-
-                    // Send the image back using NetMQ
-                    // SendNetMQImage(timestamp, ref image, should_compress_video);
+                    // Create packet metadata
                     var msg = new NetMQMessage();
-                    msg.Append(timestamp);
-                    msg.Append(Convert.ToInt16(should_compress_video));
-                    msg.Append(image);
-                    lock (socket_lock)
-                    {
-                        push_socket.TrySendMultipartMessage(msg);
-                    }
-                });
+                    msg.Append(JsonConvert.SerializeObject(metadata));
 
-                
-            } else {
-                ensureScreenSize();
-            }
+                    // Process each camera's image, compress the image, and serialize the result.
+                    // Compression is disabled for now...
+                    List<byte[]> images = state.Cameras.AsParallel().Select(cam => get_raw_image(cam, raw)).ToList();
+
+                    // Append images to message
+                    images.ForEach(image => msg.Append(image));
+
+
+                msg.Append(image);
+                lock (socket_lock)
+                {
+                    push_socket.TrySendMultipartMessage(msg);
+                }
+            });
         }
     }
 
@@ -228,6 +153,40 @@ public class CameraController : MonoBehaviour
         Debug.Log("Terminated ZMQ sockets.");
         NetMQConfig.Cleanup();
     }
+
+    // @TODO: Make allow for images to be RGB. Currently only allows for Gray output.
+    public byte[] get_raw_image(Camera_t cam, byte[] raw)
+    {
+        // Allocate variables
+        byte R, G, B;
+        byte[] output = new byte[cam.channels * state.cam_width * state.cam_height];
+        
+        // Figure out where camera data starts and ends
+        int y_start = cam.output_index * state.cam_height;
+        int y_end = (cam.output_index+1) * state.cam_height;
+
+        // Iterate through pixels in the source image
+        for (int y = y_start; y < y_end; y++)
+        {
+            int y_inv = state.screen_height - y - 1;
+            for (int x = 0; x < state.screen_width; x++)
+            {
+                // Get RGB values
+                R = raw[((y * state.screen_width) + x) * 3];
+                G = raw[((y * state.screen_width) + x) * 3 + 1];
+                B = raw[((y * state.screen_width) + x) * 3 + 2];
+
+                // Grayscale conversion.
+                // https://www.mathworks.com/help/matlab/ref/rgb2gray.html
+                // Use of Math.Min for overflow handling without try/catch statement.
+                // NOTE: This does not round correctly (just truncates), since rounding is SLOW (-10FPS)
+                output[(y_inv * state.screen_width) + x] = (byte)Math.Min(0.2989 * R + 0.5870 * G + 0.1140 * B, byte.MaxValue);
+            }
+        }
+
+        return output;
+    }
+
 
 
 
@@ -247,7 +206,7 @@ public class CameraController : MonoBehaviour
         // Blocking receive for a message
         msg = pull_socket.ReceiveMultipartMessage();
         // Check if this is the latest message
-        while (pull_socket.TryReceiveMultipartMessage(ref new_msg));
+        while (pull_socket.TryReceiveMultipartMessage(ref new_msg)) ;
 
         // Check that we got the whole message
         if (new_msg.FrameCount >= msg.FrameCount)
@@ -261,14 +220,12 @@ public class CameraController : MonoBehaviour
         }
 
         // Get scene state from LCM
-        StateMessage_t state =  JsonConvert.DeserializeObject<StateMessage_t>(msg[1].ConvertToString());
+        StateMessage_t state = JsonConvert.DeserializeObject<StateMessage_t>(msg[1].ConvertToString());
+
         // Update position of game objects.
         updateObjectPositions();
         // Make sure that all objects are initialized properly
         initializeObjects();
-
-        // Make sure that the rest of the program knows that we are connected.
-        connected = true;
     }
 
 
@@ -278,7 +235,7 @@ public class CameraController : MonoBehaviour
         foreach (Camera_t obj_state in state.Cameras)
         {
             // Get camera game object 
-            GameObject obj = obj_state.get_gameobject(objects, camera_template);
+            GameObject obj = internal_state.get_gameobject(obj_state.ID, camera_template);
             // Apply translation and rotation
             obj.transform.SetPositionAndRotation(ListToVector3(obj_state.position), ListToQuaternion(obj_state.rotation));
 
@@ -288,7 +245,7 @@ public class CameraController : MonoBehaviour
         foreach (Window_t obj_state in state.Windows)
         {
             // Get camera game object 
-            GameObject obj = obj_state.get_gameobject(objects, window_template);
+            GameObject obj = internal_state.get_gameobject(obj_state.ID, window_template);
             // Apply translation and rotation
             obj.transform.SetPositionAndRotation(ListToVector3(obj_state.position), ListToQuaternion(obj_state.rotation));
 
@@ -297,26 +254,63 @@ public class CameraController : MonoBehaviour
 
     // Tries to initialize uninitialized objects multiple times until the object is initialized.
     // When everything is initialized, this function will NOP.
-    void initializeObjects() {
+    void initializeObjects()
+    {
 
-        //foreach (Window_t obj_state in state.Windows where )
+        // Initialize Screen & keep track of frames to skip
+        internal_state.screen_skip_frames = Math.Max(0, internal_state.screen_skip_frames - 1);
 
+        if (!internal_state.screen_initialized)
+        {
+            // Set the max framerate
+            Application.targetFrameRate = max_framerate;
+            // initialize the display to a window that fits all cameras
+            Screen.SetResolution(state.screen_width, state.screen_height, false);
+            // Set render texture to the correct size
+            rendered_frame = new Texture2D(state.screen_width, state.screen_height, TextureFormat.RGB24, false, true);
+            // Discard this frame, since changes do not take effect until the next frame.
+            internal_state.screen_skip_frames += 1;
+            internal_state.screen_initialized = true;
+        }
 
-        //    if (is_window)
-        //        {
-        //            // Set window size
-        //            obj.transform.localScale = window_dimensions;
-        //            // set window color
-        //            Color hsv_color = Color.HSVToRGB(window_hsv[0], window_hsv[1], window_hsv[2]);
-        //            obj.GetComponentInChildren<MeshRenderer>().material.SetColor("_EmissionColor", hsv_color);
-        //        }
+        // Initialize windows
+        state.Windows.Where(obj => !internal_state.is_initialized(obj.ID)).ToList().ForEach(
+            obj_state =>
+            {
+                // Get objects
+                ObjectState_t internal_object_state = internal_state.get_wrapper_object(obj_state.ID, window_template);
+                GameObject obj = internal_object_state.game_obj;
+                // Set window size
+                obj.transform.localScale = ListToVector3(obj_state.size);
+                // set window color
+                obj.GetComponentInChildren<MeshRenderer>().material.SetColor("_EmissionColor", ListHSVToColor(obj_state.color));
+                // Mark initialized.
+                internal_object_state.initialized = true;
+            }
+        );
 
-        //        // Save the object.
-        //        objects.Add(ID, new SimulationObj() { ID = ID, obj = obj, render_index = render_index });
+        // Initialize Camera objects if screen is ready to render.
+        if (internal_state.screen_initialized && internal_state.screen_skip_frames == 0)
+        {
+            state.Cameras.Where(obj => !internal_state.is_initialized(obj.ID)).ToList().ForEach(
+                obj_state =>
+                {
+                    // Get object
+                    ObjectState_t internal_object_state = internal_state.get_wrapper_object(obj_state.ID, camera_template);
+                    GameObject obj = internal_object_state.game_obj;
+                    // Make sure camera renders to the correct portion of the screen.
+                    obj.GetComponent<Camera>().pixelRect = new Rect(0, state.cam_height * (state.num_cameras - obj_state.output_index - 1), state.cam_width, state.cam_height);
+                    // Ensure FOV is set for camera.
+                    obj.GetComponent<Camera>().fieldOfView = state.cam_vertical_fov;
+                    // @TODO: Ensure that post-processing profiles are correct (RGB vs Gray)
 
-        throw new NotImplementedException();
-
+                    // enable Camera.
+                    obj.SetActive(true);
+                    // Log that camera is initialized
+                    internal_object_state.initialized = true;
+                }
+            );
+        }
     }
-
 
 }
--- FlightGoggles/Scripts/MessageSpec.cs
+++ FlightGoggles/Scripts/MessageSpec.cs
@@ -8,6 +8,73 @@ using System.Linq;
 
 namespace MessageSpec
 {
+
+    // =============================
+    // INTERNAL Message definitions
+    // =============================
+    // For storing unity's internal state
+    // E.g. are objects initialized, should this frame be rendered, etc.
+    public class UnityState_t
+    {
+        private Dictionary<string, ObjectState_t> objects;
+
+        // Screen state
+        public bool screen_initialized { get; set; } = false;
+        public int screen_skip_frames { get; set; } = 0;
+
+        // Advanced getters/setters
+        // Ensure object exists.
+        public void ensure_object_exists(string ID, GameObject template)
+        {
+            if (!objects.ContainsKey(ID)) {
+                // Create and save object from template
+                objects[ID] = new ObjectState_t(template);
+            }
+        }
+
+        // Get Wrapper object
+        public ObjectState_t get_wrapper_object(string ID, GameObject template)
+        {
+            ensure_object_exists(ID, template);
+            return objects[ID];
+        }
+        // Get Wrapper object
+        public GameObject get_gameobject(string ID, GameObject template)
+        {
+            return get_wrapper_object(ID, template).game_obj;
+        }
+        // Check if object is initialized
+        public bool is_initialized(string ID)
+        {
+            bool is_initialized = false;
+            if (objects.ContainsKey(ID))
+            {
+                is_initialized = objects[ID].initialized;
+            }
+            return is_initialized;
+        }
+        // Constructor
+        public UnityState_t()
+        {
+            objects = new Dictionary<string, ObjectState_t>() { };
+        }
+    }
+
+    // Keeps track of gameobjects and their initialization and instantiation.
+    public class ObjectState_t
+    {
+        public bool initialized { get; set; } = false;
+        public GameObject game_obj { get; set; }
+        public GameObject template { get; set; }
+        // Constructor
+        public ObjectState_t(GameObject template)
+        {
+            this.game_obj = GameObject.Instantiate(template);
+            this.template = template;
+        }
+
+    }
+
     // =============================
     // INCOMING Message definitions
     // =============================
@@ -18,14 +85,16 @@ namespace MessageSpec
         public int cam_width { get; set; }
         public int cam_height { get; set; }
         public float cam_vertical_fov { get; set; }
+        public bool compress_image { get; set; }
         // Object state update
         public IList<Camera_t> Cameras { get; set; }
         public IList<Window_t> Windows { get; set; }
-
         // Additional getters
         public int num_cameras { get { return Cameras.Count(); } }
-        public int rendered_image_width { get { return cam_width; } }
-        public int rendered_image_height { get { return cam_height * num_cameras; } }
+        public int screen_width { get { return cam_width; } }
+        public int screen_height { get { return cam_height * num_cameras; } }
+
+
     }
 
     // Camera class for decoding the ZMQ messages.
@@ -34,7 +103,6 @@ namespace MessageSpec
         public string ID { get; set; }
         public IList<float> position { get; set; }
         public IList<float> rotation { get; set; }
-        public bool initialized { get; set; } = false; // default to false.
         // Metadata
         public int channels { get; set; }
         public bool has_depth { get; set; }
@@ -43,20 +111,6 @@ namespace MessageSpec
         // Additional getters
         public bool is_grayscale { get { return (channels == 1) && (!has_depth); } }
 
-        // Get Object, creating a new one if neccessary.
-        public GameObject get_gameobject(Dictionary<string, GameObject> objects, GameObject template) {
-            GameObject obj;
-            if (objects.ContainsKey(ID))
-            {
-                obj = objects[ID];
-            } else
-            {
-                // Create object from template
-                obj = GameObject.Instantiate(template);
-                
-            }
-            return obj;
-        }
     }
 
     // Window class for decoding the ZMQ messages.
@@ -65,28 +119,11 @@ namespace MessageSpec
         public string ID { get; set; }
         public IList<float> position { get; set; }
         public IList<float> rotation { get; set; }
-        public bool initialized { get; set; } = false; // default to false.
         // Metadata
         public IList<float> color { get; set; }
         public IList<float> size { get; set; }
-        // Get Object, creating a new one if neccessary.
-        public GameObject get_gameobject(Dictionary<string, GameObject> objects, GameObject template)
-        {
-            GameObject obj;
-            if (objects.ContainsKey(ID))
-            {
-                obj = objects[ID];
-            }
-            else
-            {
-                // Create object from template
-                obj = GameObject.Instantiate(template);
-
-            }
-            return obj;
-        }
     }
-    }
+    
 
 
     // =============================
@@ -103,6 +140,16 @@ namespace MessageSpec
         // Additional metadata for helping with the deserialization process.
         public IList<string> camera_IDs { get; set; }
         public IList<int> channels { get; set; }
-}
+
+        public RenderMetadata_t(StateMessage_t state)
+        {
+            utime = state.utime;
+            is_compressed = state.compress_image;
+            cam_width = state.cam_width;
+            cam_height = state.cam_height;
+            camera_IDs = state.Cameras.Select(obj => obj.ID).ToList();
+            channels = state.Cameras.Select(obj => obj.channels).ToList();
+        }
+    }
 
 }
