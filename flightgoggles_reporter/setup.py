191b7da3b6723d776d340b26e4aa92547de97aac
Winter Guerra <winterg@mit.edu>
2019-03-16 13:55:39 -0400

    Added support for `catkin_make install`. Previously, only commands from `catkin_tools` were supported.

--- flightgoggles_reporter/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-d = generate_distutils_setup(
-    packages=['flightgoggles_reporter'],
-    package_dir={'': 'src'}
-)
-
-setup(**d)
