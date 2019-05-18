16b108e4a290b736cf2cb982b231a90818c6732c
Winter Guerra <winterg@mit.edu>
2019-05-17 23:17:28 -0400

    Fixed BB dataset baseline. Fixed render monitor. Fixed render offset for BB dataset.

--- flightgoggles_render_monitor/scripts/flightgoggles_render_monitor.py
+++ flightgoggles_render_monitor/scripts/flightgoggles_render_monitor.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python
-import rospy
+#!/usr/bin/env python2
+import rospy, time
 from sensor_msgs.msg import CameraInfo
 
 '''
@@ -11,23 +11,44 @@ This node can be required by launchfiles that should die after the renderer is f
 Ex: this node can tell the blackbirdDataset.launch launchfile to exit when a particular trajectory is done rendering.
 '''
 
-has_started_receiving_data = False
-num_seconds_without_data = 0
-max_num_seconds_without_data = 6
+
+class FlightGogglesRenderMonitor():
+
+    def __init__(self):
+        self.has_started_receiving_data = False
+        self.received_data_since_last_check = False
+        self.num_seconds_without_data = 0
+        self.max_num_seconds_without_data = 5
+
+        pub = rospy.Subscriber('/uav/camera/left/camera_info', CameraInfo, self.cameraInfoCallback)
 
 
-def cameraInfoCallback(data):
-    has_started_receiving_data = True
-    
+    def cameraInfoCallback(self, data):
+        self.received_data_since_last_check = True
+        self.has_started_receiving_data = True
+        print "Received data!"
 
-def flightGogglesRenderMonitor():
-    pub = rospy.Subscriber('/uav/camera/left/camera_info', CameraInfo, cameraInfoCallback)
-    rospy.init_node('flightgoggles_render_monitor', anonymous=True)
-    rate = rospy.Rate(1) # 1Hz
-    
-    while not rospy.is_shutdown():
-        # Check that 
-        rate.sleep()
+
+    def spin(self):
+
+        while not rospy.is_shutdown():
+            print "loop start!"
+            if (self.has_started_receiving_data):
+                if (self.received_data_since_last_check):
+                    self.num_seconds_without_data = 0
+                    self.received_data_since_last_check = False
+                    print "yes data"
+                else:
+                    self.num_seconds_without_data+=1
+                    print "no data"
+
+            if (self.num_seconds_without_data > self.max_num_seconds_without_data):
+                print "prog end"
+                rospy.signal_shutdown("Render monitor has detected a long pause in render output. Shutting down.")
+            # Cannot use ROS time because this will hang indefinitely if using simtime and simtime stops.
+            time.sleep(1)
 
 if __name__ == '__main__':
-    flightGogglesRenderMonitor()
+    node = FlightGogglesRenderMonitor()
+    rospy.init_node('flightgoggles_render_monitor', anonymous=True)
+    node.spin()
